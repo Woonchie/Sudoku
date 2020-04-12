308041a1c9d17b2f0ddf49a042db3c06f0c5c6a3
michael lopez <michael_lopez1997@hotmail.com>
2020-04-12 11:55:34 -0400

    Create the "Tap Anywhere" title menu, and created a main menu with "Start New Game", "Load Game", "Previous"

--- app/src/main/java/com/example/sudoku/MainMenuFragment.java
+++ app/src/main/java/com/example/sudoku/MainMenuFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class MainMenuFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,17 +17,17 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_mainmenu, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_return_to_title).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(MainMenuFragment.this)
+                        .navigate(R.id.action_MainMenuFragment_to_TitleFragment);
             }
         });
     }
--- app/src/main/java/com/example/sudoku/TitleFragment.java
+++ app/src/main/java/com/example/sudoku/TitleFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+public class TitleFragment extends Fragment {
+
+    private RelativeLayout mainLayout;
 
     @Override
     public View onCreateView(
@@ -17,17 +20,18 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_title, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        //Make the whole screen clickable
+        view.findViewById(R.id.tap_anywhere_layout).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(TitleFragment.this)
+                        .navigate(R.id.action_TitleFragment_to_MainMenuFragment);
             }
         });
     }
